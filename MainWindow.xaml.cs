d6f1c2068c0b7a5c66005ca4bd893d0928ad5a10
user <user@WIN-SOM11LA14AP>
2024-12-18 21:57:05 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Product
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> Products { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Products = new ObservableCollection<Product>()
+            {
+                new Product { Image = "Фото", Name = "Название", Description = "Описание", Manufacturer = "Производитель", Price = "Цена", StockQuantity = "Наличие на складе"},
+                new Product { Image = "Фото", Name = "Название", Description = "Описание", Manufacturer = "Производитель", Price = "Цена", StockQuantity = "Наличие на складе"},
+                new Product { Image = "Фото", Name = "Название", Description = "Описание", Manufacturer = "Производитель", Price = "Цена", StockQuantity = "Наличие на складе"},
+                new Product { Image = "Фото", Name = "Название", Description = "Описание", Manufacturer = "Производитель", Price = "Цена", StockQuantity = "Наличие на складе"},
+                new Product { Image = "Фото", Name = "Название", Description = "Описание", Manufacturer = "Производитель", Price = "Цена", StockQuantity = "Наличие на складе"},
+                new Product { Image = "Фото", Name = "Название", Description = "Описание", Manufacturer = "Производитель", Price = "Цена", StockQuantity = "Наличие на складе"},
+                new Product { Image = "Фото", Name = "Название", Description = "Описание", Manufacturer = "Производитель", Price = "Цена", StockQuantity = "Наличие на складе"}
+            };
+        }
+    }
+}
